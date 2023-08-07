18002eead620b56e694dfe88e7da1216d9033ba6
seoyeon_jang <seoyeon_jang@tmax.co.kr>
2023-08-07 23:52:57 +0900

    :sparkles: [feat] route에 홈 추가

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,5 +1,3 @@
-import React from "react";
-
 import { createBrowserRouter } from "react-router-dom";
 import { styled } from "styled-components";
 
@@ -27,7 +25,10 @@ const router = createBrowserRouter([
       {
         path: "",
         index: true,
-        Component: TodoPage,
+        async lazy() {
+          const Home = await import("@pages/Home");
+          return { Component: Home.default };
+        },
       },
       {
         path: "login",
