86d1ef1d703033ddf510e2666bc0210cdbd14bfe
seoyeon_jang <seoyeon_jang@tmax.co.kr>
2023-08-08 13:42:36 +0900

    :sparkles: [feat] 서치 화면 - 서치바

--- src/pages/Search/index.tsx
+++ src/pages/Search/index.tsx
@@ -1,7 +1,67 @@
-import React, { ReactElement } from "react";
+import React, { ReactElement, useCallback, useEffect, useRef } from "react";
+
+import { ArrowUpward } from "@mui/icons-material";
+import { IconButton, TextField, TextFieldProps } from "@mui/material";
+import styled from "styled-components";
+
+import { useStore } from "@/stores";
+
+const ContentWrapper = styled.main`
+  position: absolute;
+  top: 56px;
+  width: 100%;
+  height: calc(100% - 56px);
+  display: flex;
+  flex-direction: column;
+
+  .search-area {
+    padding: 0.5rem 1rem;
+    display: inline-flex;
+    gap: 0.5rem;
+
+    .MuiInputBase-root {
+      background-color: #ffffff;
+      border-radius: 33px;
+      border: 1px solid #3a00e5;
+    }
+
+    .MuiIconButton-root {
+      background-color: #3a00e5;
+      color: #ffffff;
+    }
+  }
+`;
 
 const Search = (): ReactElement => {
-  return <div>서치 화면</div>;
+  const { mainStore } = useStore();
+  const inputRef = useRef<TextFieldProps | null>(null);
+
+  /** @TODO list GET api */
+
+  const handleClickSearchButton = useCallback(() => {
+    mainStore.setSearchWord(inputRef.current?.value as string);
+  }, []);
+
+  useEffect(() => {
+    if (inputRef) {
+      if (inputRef.current) inputRef.current.value = mainStore.searchWord;
+    }
+  }, [inputRef]);
+
+  return (
+    <ContentWrapper>
+      <section className="search-area">
+        <TextField fullWidth size="small" inputRef={inputRef} />
+        <IconButton onClick={handleClickSearchButton}>
+          <ArrowUpward />
+        </IconButton>
+      </section>
+      <section>tab</section>
+      <section>top</section>
+      <section>list</section>
+      <section>pagination</section>
+    </ContentWrapper>
+  );
 };
 
 export default Search;
