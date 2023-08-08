6a1f0f53940703ad949368bb829573ad68b3fe8c
seoyeon_jang <seoyeon_jang@tmax.co.kr>
2023-08-08 13:12:20 +0900

    :wrench: store 관련 컨피그 수정

--- craco.config.js
+++ craco.config.js
@@ -5,6 +5,17 @@ module.exports = {
     alias: {
       "@": path.resolve(__dirname, "src/"),
       "@pages": path.resolve(__dirname, "src/pages"),
+      "@stores": path.resolve(__dirname, "src/stores"),
+    },
+  },
+  babel: {
+    plugins: [
+      ["@babel/plugin-transform-class-properties", { loose: false }],
+      ["@babel/plugin-transform-private-methods", { loose: false }],
+      ["@babel/plugin-transform-private-property-in-object", { loose: false }],
+    ],
+    assumptions: {
+      setPublicClassFields: false,
     },
   },
 };
