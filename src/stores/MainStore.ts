4b9075a06d6907e718d22547cbce290db0d544fa
seoyeon_jang <seoyeon_jang@tmax.co.kr>
2023-08-08 15:56:04 +0900

    :sparkles: store에 탭 값 추가

--- src/stores/MainStore.ts
+++ src/stores/MainStore.ts
@@ -1,16 +1,25 @@
 import { makeAutoObservable } from "mobx";
 
+import { SearchTabType } from "@/interface/search";
+
 class MainStore {
   searchWord: string;
 
+  selectedTab: SearchTabType;
+
   constructor() {
     this.searchWord = "";
+    this.selectedTab = "PRECEDENT";
 
     makeAutoObservable(this);
   }
 
   setSearchWord(value: string) {
     this.searchWord = value;
+  }
+
+  setSelectedTab(value: SearchTabType) {
+    this.selectedTab = value;
   }
 }
 
