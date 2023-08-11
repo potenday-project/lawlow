fadc1d672a4865b25a58dd9071ce08033ec76c91
seoyeon_jang <seoyeon_jang@tmax.co.kr>
2023-08-12 05:53:40 +0900

    :wrench: react-query 사용 준비

--- src/App.tsx
+++ src/App.tsx
@@ -4,18 +4,37 @@ import "./App.css";
 
 import CssBaseline from "@mui/material/CssBaseline";
 import { ThemeProvider } from "@mui/material/styles";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { RouterProvider } from "react-router-dom";
 
 import themes from "@style/theme";
 
 import router from "./routes";
 
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      retry: 0,
+      useErrorBoundary: true,
+      refetchOnWindowFocus: false,
+      suspense: true,
+    },
+    mutations: {
+      useErrorBoundary: true,
+    },
+  },
+});
+
 const App = () => {
   return (
-    <ThemeProvider theme={themes.light}>
-      <CssBaseline />
-      <RouterProvider router={router} />
-    </ThemeProvider>
+    <QueryClientProvider client={queryClient}>
+      <ThemeProvider theme={themes.light}>
+        <CssBaseline />
+        <RouterProvider router={router} />
+      </ThemeProvider>
+      <ReactQueryDevtools initialIsOpen />
+    </QueryClientProvider>
   );
 };
 
